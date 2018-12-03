7247e7624fe9ed4b97fdcae083e56a7c0a0b73e9
qcm123 <2746646865@qq.com>
2018-12-03 19:58:51 +0800

    2.测试使用

---
+++ src/main/java/com/aaa/gjj/dao/TestDao.java
@@ -0,0 +1,10 @@
+package com.aaa.gjj.dao;
+
+/**
+ * className:TestDao
+ * discription:
+ * author:qcm
+ * createTime:2018-12-03 19:58
+ */
+public class TestDao {
+}
---
+++ src/main/java/com/aaa/gjj/service/TestService.java
@@ -0,0 +1,10 @@
+package com.aaa.gjj.service;
+
+/**
+ * className:TestService
+ * discription:
+ * author:qcm
+ * createTime:2018-12-03 19:58
+ */
+public class TestService {
+}
