1484d8996e7f88517c73ad3d6440e88894b0cf22
qcm123 <2746646865@qq.com>
2018-12-06 21:10:03 +0800

    shiro提交--曲从明

---
+++ src/main/java/com/aaa/gjj/service/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package com.aaa.gjj.service;
+
+
+import com.aaa.gjj.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * className:UserServiceImpl
+ * discription:
+ * author:luRuiHua
+ * createTime:2018-12-01 14:51
+ */
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserDao userDao;
+
+    /**
+     * 查询用户
+     * @return
+     */
+    @Override
+    public List<Map> selectUserList(String userName) {
+        return userDao.selectUserList(userName);
+    }
+}
